03c2d33b24edbc684d3058c18448deabbdcdd09f
yhz05 <yhz05@gmail.com>
2026-03-18 11:25:32 +0200

    gui of the main window

--- shell-gui/MainWindow.axaml.cs
+++ shell-gui/MainWindow.axaml.cs
@@ -1,11 +1,24 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
-namespace shell_gui;
-
-public partial class MainWindow : Window
+namespace shell_gui
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Button click handler (logic comes later)
+        private void RunButton_Click(object? sender, RoutedEventArgs e)
+        {
+            string command = CommandTextBox.Text ?? "";
+
+            // For now just display it in output box
+            OutputTextBox.Text += $"> {command}\n";
+
+            // TODO: later we will execute the command and show real output
+        }
     }
 }
